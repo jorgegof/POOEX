c4ad46361c5ff0f975b54330681df71294956803
jorgegof <jorge.filho@academico.ifpb.edu.br>
2017-06-29 08:39:53 -0300

    AulaGithub

---
+++ Heranca/src/heranca/Funcionario.java
@@ -0,0 +1,70 @@
+package heranca;
+
+public class Funcionario {
+
+	private String CPF;
+	private String nome;
+	private String endereco;
+	
+	public Funcionario(String cPF, String nome, String endereco) {
+
+		CPF = cPF;
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String toString() {
+		return "Funcionario [CPF=" + CPF + ", nome=" + nome + ", endereco=" + endereco + "] ";
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (CPF == null) {
+			if (other.CPF != null)
+				return false;
+		} else if (!CPF.equals(other.CPF))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+}
---
+++ Heranca/src/heranca/Teste.java
@@ -0,0 +1,16 @@
+package heranca;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Medico m = new Medico("123456","Jorge","Av Joao",852456);
+		
+		System.out.println(m.toString());
+		
+		System.out.println(m.getCrm());
+		
+
+	}
+
+}
